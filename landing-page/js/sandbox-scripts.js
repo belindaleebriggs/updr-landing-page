2dcce616448eb1ced3b393d611dc8f8f26199735
Belinda Briggs <belinda@belindaleebriggs.com>
2020-08-29 15:04:47 -0400

    docs: remove old and update resource js
    - created a js file to hold scripts that will be useful in this project.
    - removed an old doc.

---
+++ landing-page/js/sandbox-scripts.js
@@ -0,0 +1,5 @@
+// This can be used to change the color of the active section and the newly inactive sections
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
+}
