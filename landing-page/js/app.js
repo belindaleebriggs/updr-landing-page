6aab3805653be5755cc884bc14fd79de46729174
Belinda Briggs <belinda@belindaleebriggs.com>
2020-08-29 16:56:21 -0400

    feat: Create Nav function
    - function written but is not working

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,11 +30,22 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
-
+function createNav() {
+// Build nav menu using document fragment
+// get sections so we can access their data-nav values to create the section links
+ var sections = document.querySelectorAll('section');
+ const navDocFrag = document.createDocumentFragment();
+ for (let sxn = 0; sxn < sections.length; sxn ++) {
+    const newElement = document.createElement("li");
+    newElement.innerText = sections[sxn].data-nav;
+    myDocFrag.appendChild(newElement);
+      }
+  document.body.appendChild(myDocFrag);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,13 +56,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
--- landing-page/js/sandbox-scripts.js
+++ landing-page/js/sandbox-scripts.js
@@ -3,3 +3,30 @@ function changeColor(newColor) {
   var elem = document.getElementById('para');
   elem.style.color = newColor;
 }
+
+// This can be used to to retrieve and element by it's ID and then if it's class is currently active, it will set it to inactive, if inactive it sets it to active.  Not entirely what we need because we need to set the active to inactive and set the just clicked item to active.
+
+let el = document.getElementById('item');
+
+if (el.className === 'active'){
+  el.className = 'inactive';
+} else {
+  el.className = 'active';
+}
+
+// Use this to modify above, using toggle will remove a class if it's already set for an element, otherwise it will add setInterval(function () {
+
+div.classList.toggle("active"););
+
+function createNav() {
+// Build nav menu using document fragment
+// get sections so we can access their data-nav values to create the section links
+ var sections = document.querySelectorAll('section');
+ const navDocFrag = document.createDocumentFragment();
+ for (let sxn = 0; sxn < sections.length; sxn ++) {
+    const newElement = document.createElement("li");
+    newElement.innerText = sections[sxn].data-nav;
+    myDocFrag.appendChild(newElement);
+      }
+  document.body.appendChild(myDocFrag);
+}
